7c9baff2fd4a95464226e2c1fc02be4ccc61e52b
Nanguage <Nanguage@yahoo.com>
2023-03-09 16:45:42 +0800

    remove .deploy_docs.yml

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -20,3 +20,4 @@ if __name__ == '__main__':
     if 'no' == '{{ cookiecutter.need_docs }}':
         remove_dir('docs')
         remove_file('.readthedocs.yml')
+        remove_file('.github/workflows/.deploy_docs.yml')
